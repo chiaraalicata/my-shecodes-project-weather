6f18643d8ff9cac2e60a1c4a8f45b56794e61f3d
chiaraalicata <74967879+chiaraalicata@users.noreply.github.com>
2022-03-13 20:03:53 +0100

    changing the last details

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
+// display real date of the year, week and time
+
 let now = new Date();
-
 let currentTime = document.querySelector("#day-and-time");
-
 let hours = now.getHours();
 if (hours < 10) {
   hours = `0${hours}`;
@@ -10,7 +10,6 @@ let minutes = now.getMinutes();
 if (minutes < 10) {
   minutes = `0${minutes}`;
 }
-
 let days = [
   "Sunday",
   "Monday",
@@ -20,9 +19,7 @@ let days = [
   "Friday",
   "Saturday",
 ];
-
 let day = days[now.getDay()];
-
 let months = [
   "January",
   "February",
@@ -40,9 +37,10 @@ let months = [
 let month = months[now.getMonth()];
 let year = now.getFullYear();
 let date = now.getDate();
-
 currentTime.innerHTML = `${day}, ${month} ${date} </br> ${year} </br> 
 ${hours}:${minutes}`;
+
+// forecast for the next 4 days
 
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
@@ -51,19 +49,17 @@ function formatDay(timestamp) {
 
   return days[day];
 }
-
 function getForecast(coordinates) {
   let apiKey = "7cf24a2441259043e3f7ca4927b8b80f";
   let apiUrl = `http://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude={part}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
-
 function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#weather-forecast");
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 4) {
+    if (index > 0 && index < 5) {
       forecastHTML =
         forecastHTML +
         `   <div class="col-2 next-weather">
@@ -85,21 +81,25 @@ function displayForecast(response) {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-// integration of real data JS-API
+
+// integration of real data API
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#actual-temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description-weather");
+  let feelsLike = document.querySelector("#feels-like");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let iconElement = document.querySelector("#icon");
 
   celsiusTemperature = response.data.main.temp;
+  getFeelsLike = response.data.main.feels_like;
 
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  cityElement.innerHTML = response.data.name;
+  cityElement.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  feelsLike.innerHTML = `Feels Like: ${Math.round(getFeelsLike)}°`;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   iconElement.setAttribute(
@@ -110,18 +110,18 @@ function displayTemperature(response) {
 
   getForecast(response.data.coord);
 }
-
 function search(city) {
   let apiKey = "7cf24a2441259043e3f7ca4927b8b80f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
-
 function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
 }
+
+// Unit conversion buttons
 
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
@@ -131,7 +131,6 @@ function displayFahrenheitTemperature(event) {
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
-
 function displayCelsiusTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#actual-temperature");
@@ -139,16 +138,53 @@ function displayCelsiusTemperature(event) {
   fahrenheitLink.classList.remove("active");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
-
 let celsiusTemperature = null;
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-
 let fahrenheitLink = document.querySelector("#fahrenheit");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
 let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+// get the current position
+
+function getCurrentPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=7cf24a2441259043e3f7ca4927b8b80f`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function clickCurrent(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getCurrentPosition);
+}
+let currentButton = document.querySelector("#current-location");
+currentButton.addEventListener("click", clickCurrent);
+
+// change the funny phrase based on the weather
+
+function changeFunnyPhrase(phrase) {
+  if (
+    phrase === "Clear" &&
+    phrase === "Few clouds" &&
+    phrase === "Scattered clouds"
+  ) {
+    return "Today is the perfect day to take a walk outside 🌻";
+  }
+  if (phrase === "Broken clouds" && phrase === "Drizzle" && phrase === "Rain") {
+    return "It's time for 💃🏽 in the 🌧, but don't forget your ☂️";
+  }
+  if (phrase === "Thunderstorm") {
+    return "Maybe it's better to stay 🏠. Read a 📖 and enjoy a cup of hot 🍵";
+  }
+  if (phrase === "Snow") {
+    return "Do you want to built a ☃️?";
+  }
+  phrase = response.data.weather[0].description;
+  let changePhrase = document.querySelector("#funny-phrase");
+  changePhrase.innerHTML = phrase;
+}
+
+// default city on the app
 
 search("San Francisco");
